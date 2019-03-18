8804ddfa97f45c47628a6a87d30a4eb5ec159e88
Blaney83 <professionallaney@gmail.com>
2019-03-17 19:31:09 -0700

    Euclidian Distance added.

--- java/EuclidianDistance.java
+++ java/EuclidianDistance.java
@@ -2,4 +2,26 @@ package formucode;
 
 public class EuclidianDistance {
 
+	public double calculate(final double[] pointOne, final double[] pointTwo) {
+
+		if (pointOne.length != pointTwo.length) {
+			
+			return -1;
+			
+		}
+
+		double currSum = 0;
+		
+		for (int i = 0; i < pointOne.length; i++) {
+			
+			double diff = (pointOne[i] - pointTwo[i]);
+			
+			currSum += (diff * diff);
+
+		}
+		
+		double finalValue = Math.sqrt(currSum);
+		
+		return finalValue;
+	}
 }
